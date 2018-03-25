96280bda4225b4b1a2d8d4a05db5b1fe3904d982
Lukygx <34741388+Lukygx@users.noreply.github.com>
2018-03-25 21:04:12 +0200

    Update servo_handuino_ind_pendant.ino

--- Ressources/Programmes/servo_handuino_ind_pendant.ino
+++ Ressources/Programmes/servo_handuino_ind_pendant.ino
@@ -1,11 +1,4 @@
-/*
- Controlling a servo position using a potentiometer (variable resistor)
- by Michal Rinott <http://people.interaction-ivrea.it/m.rinott>
 
- modified on 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Knob
-*/
 
 #include <Servo.h>
 
